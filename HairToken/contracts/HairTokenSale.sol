0e5b1c00c43c923a7c3cd49e33d7fb2fb30505e4
averbinnen <109697460+averbinnen@users.noreply.github.com>
2023-01-05 15:24:26 -0500

    Update HairTokenSale.sol

--- HairToken/contracts/HairTokenSale.sol
+++ HairToken/contracts/HairTokenSale.sol
@@ -6,7 +6,7 @@ pragma solidity ^0.8.17;
 import "./HairToken.sol";
 
 contract HairTokenSale {
-    address admin;
+    address payable admin;
     HairToken public tokenContract;
     uint256 public tokenPrice;
     uint256 public tokensSold;
@@ -14,23 +14,23 @@ contract HairTokenSale {
     event Sell(address _buyer, uint256 _amount);
 
     function HairAuction(HairToken _tokenContract, uint256 _tokenPrice) public {
-        admin = msg.sender;
+        admin = payable(msg.sender);
         tokenContract = _tokenContract;
         tokenPrice = _tokenPrice;
     }
 
-    function multiply(uint x, uint y) internal pure returns (uint z) {
+    function multiply(uint256 x, uint256 y) internal pure returns (uint256 z) {
         require(y == 0 || (z = x * y) / y == x);
     }
 
     function buyTokens(uint256 _numberOfTokens) public payable {
         require(msg.value == multiply(_numberOfTokens, tokenPrice));
-        require(tokenContract.balanceOf(address(this) >= _numberOfTokens));
+        require(tokenContract.balanceOf(address(this)) >= _numberOfTokens);
         require(tokenContract.transfer(msg.sender, _numberOfTokens));
 
         tokensSold += _numberOfTokens;
 
-        Sell(msg.sender, _numberOfTokens);
+        emit Sell(msg.sender, _numberOfTokens);
     }
 
     function endSale() public {
@@ -46,3 +46,4 @@ contract HairTokenSale {
         admin.transfer(address(this).balance);
     }
 }
+
