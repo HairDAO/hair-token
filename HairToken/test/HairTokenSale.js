05272732ad0241e7f76aedcd94e54499e80e6d45
averbinnen <109697460+averbinnen@users.noreply.github.com>
2023-01-06 14:11:59 -0500

    Update HairTokenSale.js

--- HairToken/test/HairTokenSale.js
+++ HairToken/test/HairTokenSale.js
@@ -1,27 +1,47 @@
 const { expect } = require("chai");
 const hre = require("hardhat");
 
-describe("HairAuction", () => {
-  let hairAuction;
+describe("HairTokenSale", () => {
+  let hairTokenSale;
   let hairToken;
   let owner;
   let addr1;
-  let addre2;
-  let tokenPrice = 320000; //in gwei
+  let addr2;
+  let tokenPrice = 320000000000000; //in wei
   let initSupply = 125000;
 
   before(async () => {
+    // Get the contract factory for the HairToken contract
+    const HairToken = await hre.ethers.getContractFactory("HairToken");
+    hairToken = await HairToken.deploy(initSupply);
+
     // Get the contract factory for the HairAuction contract
-    const HairAuction = await getContractFactory("HairAuction");
-    [owner, addr1, addr2] = await hardhat.ethers.getSigners();
+    const HairTokenSale = await ethers.getContractFactory("HairTokenSale");
+    [owner, addr1, addr2] = await hre.ethers.getSigners();
 
     // Deploy the contract and get a reference to it
-    hairAuction = await HairAuction.deploy([]);
+    hairTokenSale = await HairTokenSale.deploy([]);
   });
 
   describe("Deployment", function () {
-    it("Should set the right owner", async function () {
-      expect(await hairAuction.owner()).to.equal(owner.address);
+    it("initializes the contract with the correct values", function () {
+      return hairTokenSale
+        .deployed()
+        .then(function (instance) {
+          hairTokenSale = instance;
+          return hairTokenSale.address;
+        })
+        .then(function (address) {
+          expect(address).to.not.equal(0x0, "has contract address");
+          return hairTokenSale.tokenContract();
+        })
+        .then(function (address) {
+          expect(address).to.not.equal(0x0, "has token contract address");
+          return hairTokenSale.tokenPrice();
+        })
+        .then(function (price) {
+          expect(price).to.equal(tokenPrice, "token price is correct");
+        });
     });
   });
 });
