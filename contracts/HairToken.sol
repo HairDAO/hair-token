0f59e2c29bcac157123ec6cf9157337b07764635
Schmackofant <schmackofant@protonmail.com>
2023-02-08 17:51:51 +0100

    update openzepplin package and add logo

--- contracts/HairToken.sol
+++ contracts/HairToken.sol
@@ -1,5 +1,17 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.17;
+pragma solidity ^0.8.18;
+
+/*
+                        ▄▄
+▀████▀  ▀████▀▀         ██
+  ██      ██
+  ██      ██   ▄█▀██▄ ▀███ ▀███▄███
+  ██████████  ██   ██   ██   ██▀ ▀▀
+  ██      ██   ▄█████   ██   ██
+  ██      ██  ██   ██   ██   ██
+▄████▄  ▄████▄▄████▀██▄████▄████▄
+
+*/
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
@@ -7,12 +19,12 @@ import "@openzeppelin/contracts/access/Ownable.sol";
 
 contract HairToken is ERC20, ERC20Burnable, Ownable {
     // Mint initial supply and send it to the
-    // initial supply recipient (probably the DAO multisig or the sales contract)
+    // initial supply recipient
     constructor(uint256 initialSupply, address initialSupplyRecipient) ERC20("HairToken", "HAIR") {
         _mint(initialSupplyRecipient, initialSupply);
     }
 
-    // Mint new tokens (can only be called by contract owner)
+    // Mint new HAIR (can only be called by contract owner)
     function mint(address to, uint256 amount) public onlyOwner {
         _mint(to, amount);
     }
