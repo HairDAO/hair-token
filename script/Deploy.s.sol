67bb2ac8a8598661d59b68ccec281f710aec95bd
Schmackofant <schmackofant@protonmail.com>
2023-03-22 14:50:44 +0100

    fix dependencies and deploy script

--- script/Deploy.s.sol
+++ script/Deploy.s.sol
@@ -6,12 +6,12 @@ import { console } from "forge-std/console.sol";
 import { HairToken } from "../contracts/HairToken.sol";
 
 contract DeployScript is Script {
-    address recipient = address(1)
+    address recipient = address(1);
 
     function run() public {
         vm.startBroadcast();
 
-        Token token = new Token(0, recipient);
+        HairToken token = new HairToken(0, recipient);
         console.log("Token deployed at: %s", address(token));
 
         vm.stopBroadcast();
